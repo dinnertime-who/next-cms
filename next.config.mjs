ff6d4384a7d2fb7aa58cf69d2ed6bcfcc0bee43a
dinnertime-who <dinnertime.dev@gmail.com>
2024-09-21 01:50:56 +0900

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
+  },
+  logging: {
+    fetches: { fullUrl: true },
+  },
+};
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
